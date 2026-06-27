6bcd4d0334c34404dab90bf598978a8391c778ab
maonev <wongmichael9704@gmail.com>
2026-06-28 05:34:12 +0700

    seeder fixed.

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'admin_name',
         'email',
-        'password',
+        'password'
     ];
 
     /**
